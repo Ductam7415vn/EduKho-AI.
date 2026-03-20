93ac9f1661ccb7f36022badcdc0f282e36a940b6
Matcry12 <fdtywadw@gmail.com>
2026-03-20 10:58:02 +0700

    Disable composer platform check for Vercel compatibility

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitc5d76b40f01e1b38c1e0f160fdbef43d
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitc5d76b40f01e1b38c1e0f160fdbef43d', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitc5d76b40f01e1b38c1e0f160fdbef43d', 'loadClassLoader'));
